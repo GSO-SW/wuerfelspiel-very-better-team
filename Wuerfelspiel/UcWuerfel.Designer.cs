6afec43585b97c11c50bfb604a6b34aa6d841dfd
Louis <81430377+lousystems@users.noreply.github.com>
2022-04-25 12:04:15 +0200

    init UcWuerfel

---
+++ Wuerfelspiel/UcWuerfel.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace Wuerfelspiel
+{
+    partial class UcWuerfel
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWürfelNr = new System.Windows.Forms.Label();
+            this.tbxAugenAnzah = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblWürfelNr
+            // 
+            this.lblWürfelNr.AutoSize = true;
+            this.lblWürfelNr.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.9434F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWürfelNr.Location = new System.Drawing.Point(22, 0);
+            this.lblWürfelNr.Name = "lblWürfelNr";
+            this.lblWürfelNr.Size = new System.Drawing.Size(106, 29);
+            this.lblWürfelNr.TabIndex = 1;
+            this.lblWürfelNr.Text = "Würfel N";
+            // 
+            // tbxAugenAnzah
+            // 
+            this.tbxAugenAnzah.BackColor = System.Drawing.SystemColors.Window;
+            this.tbxAugenAnzah.Location = new System.Drawing.Point(27, 55);
+            this.tbxAugenAnzah.Multiline = true;
+            this.tbxAugenAnzah.Name = "tbxAugenAnzah";
+            this.tbxAugenAnzah.Size = new System.Drawing.Size(100, 20);
+            this.tbxAugenAnzah.TabIndex = 2;
+            // 
+            // UcWuerfel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tbxAugenAnzah);
+            this.Controls.Add(this.lblWürfelNr);
+            this.Name = "UcWuerfel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWürfelNr;
+        private System.Windows.Forms.TextBox tbxAugenAnzah;
+    }
+}
---
+++ Wuerfelspiel/UcWuerfel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wuerfelspiel
+{
+    public partial class UcWuerfel : UserControl
+    {
+        private Wuerfel Wuerfel;
+        private int Wert => Wuerfel.LetztesErbenis;
+
+        public UcWuerfel():this(new(6))
+        {
+          
+        }
+
+        public UcWuerfel(Wuerfel wuerfel) 
+        {
+            InitializeComponent();
+            Wuerfel = wuerfel;
+        }
+
+        private void Click() { }
+        private void DoubleClick() { }
+
+
+    }
+}
